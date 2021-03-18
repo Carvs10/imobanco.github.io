15ce596b0ae19bcd0b8ad9ddf3304f08b8232c0c
Phelipe <phelipedaniel84@gmail.com>
2021-03-17 22:05:41 -0300

    modificação na estrutura do footer

--- components/main/FooterIndex.vue
+++ components/main/FooterIndex.vue
@@ -1,33 +1,19 @@
 <template>
   <div>
-<<<<<<< HEAD
     <footer-content />
     <footer-social />
-=======
-    <!--<footer-social />-->
-    <footer-content />
->>>>>>> 4b17228fc0af99b624483ace8cd05e6d6c1e14aa
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import FooterSocial from "@/components/footer/FooterSocial.vue";
-=======
-// import FooterSocial from "@/components/footer/FooterSocial.vue";
->>>>>>> 4b17228fc0af99b624483ace8cd05e6d6c1e14aa
 import FooterContent from "@/components/footer/FooterContent.vue";
 
 export default {
   name: "FooterIndex",
   components: {
-<<<<<<< HEAD
     FooterSocial,
     FooterContent
-=======
-    //FooterSocial,
-    FooterContent,
->>>>>>> 4b17228fc0af99b624483ace8cd05e6d6c1e14aa
   },
 };
 </script>
