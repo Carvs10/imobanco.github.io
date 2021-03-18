ef43046d5dc1cb683f2cc4543f8c785853cd9e7a
dassaev-lima <dassaevnascimento@gmail.com>
2021-03-18 18:55:47 -0300

    mudando ordem das colunas da seção vantagens - boletos

--- components/boletos/VantagensBoleto.vue
+++ components/boletos/VantagensBoleto.vue
@@ -2,71 +2,76 @@
   <section class="pb-5 container">
     <b-container>
       <b-row>
-        <b-col cols="12" md="6" lg="8" class="text-left mt-5">
-          <h2>1 Boleto</h2>
+        <b-col cols="12" md="6" lg="4" class="text-right mt-5">
+          <img src="image/boletos.png" alt class="img-fluid" />
+        </b-col>
+        <b-col cols="12" md="6" lg="8" class="text-right mt-5">
+          <h2 >1 Boleto</h2>
           <span class="subtitulo">Inúmeras vantagens.</span>
           <b-row>
-            <b-col cols="12" md="6" lg="6" class="text-left">
+            <b-col cols="12" md="6" lg="6" class="text-right">
               <b-row>
-                <b-col cols="4" md="6" lg="2" class="text-left mt-5">
-                  <img src="../../static/image/icon/g224.png" alt="" class="img-fluid">
-                </b-col>
-                <b-col cols="8" md="6" lg="10" class="text-left mt-5">
+                <b-col cols="8" md="6" lg="10" class="text-right mt-5">
                     <h6>Enviamos por SMS</h6>
                     <p class="text-content">
                       Enviamos um aviso por SMS antes e d urante a data de
                       pagamento
                     </p>
                 </b-col>
+                <b-col cols="4" md="6" lg="2" class="text-right mt-5">
+                  <img src="../../static/image/icon/g224.png" alt="" class="img-fluid">
+                </b-col>
+    
               </b-row>
             </b-col>
-            <b-col cols="12" md="6" lg="6" class="text-left">
+            <b-col cols="12" md="6" lg="6" class="text-right">
               <b-row>
-                <b-col cols="4" md="6" lg="2" class="text-left mt-5">
-                  <img src="../../static/image/icon/g236.png" alt="" class="img-fluid">
-                </b-col>
-                <b-col cols="8" md="6" lg="10" class="text-left mt-5">
+                <b-col cols="8" md="6" lg="10" class="text-right mt-5">
                     <h6>Split de Pagamento</h6>
                     <p class="text-content">
                       Multibanco e automatizado
                     </p>
                 </b-col>
+                <b-col cols="4" md="6" lg="2" class="text-right mt-5">
+                  <img src="../../static/image/icon/g236.png" alt="" class="img-fluid">
+                </b-col>
+                
               </b-row>
             </b-col>
           </b-row>
           <b-row>
-            <b-col cols="12" md="6" lg="6" class="text-left">
+            <b-col cols="12" md="6" lg="6" class="text-right">
               <b-row>
-                <b-col cols="4" md="6" lg="2" class="text-left mt-2">
-                  <img src="../../static/image/icon/g244.png" alt="" class="img-fluid">
-                </b-col>
-                <b-col cols="8" md="6" lg="10" class="text-left mt-2">
+                <b-col cols="8" md="6" lg="10" class="text-right mt-2">
                     <h6>Avisamos por E-mail</h6>
                     <p class="text-content">
                       Enviamos um aviso por E-mail antes e durante a data de pagamento.
                     </p>
                 </b-col>
+                <b-col cols="4" md="6" lg="2" class="text-right mt-2">
+                  <img src="../../static/image/icon/g244.png" alt="" class="img-fluid">
+                </b-col>
+                
               </b-row>
             </b-col>
-            <b-col cols="12" md="6" lg="6" class="text-left">
+            <b-col cols="12" md="6" lg="6" class="text-right">
               <b-row>
-                <b-col cols="4" md="6" lg="2" class="text-left mt-2">
-                  <img src="../../static/image/icon/g258.png" alt="" class="img-fluid">
-                </b-col>
-                <b-col cols="8" md="6" lg="10" class="text-left mt-2">
+                <b-col cols="8" md="6" lg="10" class="text-right mt-2">
                     <h6>Sem limites de valor ou quantidade</h6>
                     <p class="text-content">
                       E mita 1 ou 1 milhão, você só paga quando seu cliente pagar.
                     </p>
                 </b-col>
+                <b-col cols="4" md="6" lg="2" class="text-right mt-2">
+                  <img src="../../static/image/icon/g258.png" alt="" class="img-fluid">
+                </b-col>
+                
               </b-row>
             </b-col>
           </b-row>
           
         </b-col>
-        <b-col cols="12" md="6" lg="4" class="text-left mt-5">
-          <img src="image/boletos.png" alt class="img-fluid" />
-        </b-col>
+        
       </b-row>
     </b-container>
   </section>
