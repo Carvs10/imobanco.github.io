05c9de1e9bc1eab813c50cfa11f47c289e8eda71
phelipequeiroz <phelipedaniel84@gmail.com>
2021-06-07 08:17:37 -0300

    slide bootstrap

--- components/inicio/ContaDigitalSection.vue
+++ components/inicio/ContaDigitalSection.vue
@@ -1,8 +1,57 @@
 <template>
   <section class="slide-top">
-    <b-container >
+    <b-container fluid >
       <b-row>
-        <b-col cols="12" md="6" lg="6" class="text-left pt-5">
+        <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-height="300"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+  
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <template #img>
+          <img
+            class="d-block img-fluid w-100"
+
+            height="300"
+            src="https://picsum.photos/1024/480/?image=55"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
+          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+    </b-carousel>
+
+  </div>
+        <!-- <b-col cols="12" md="6" lg="6" class="text-left pt-5">
           <h1 class="text-slide">Recebimentos e cobranças em um único lugar.
           </h1>
           <p class="text-subtitle ">Simplifique com o Imobanco.</p>
@@ -10,8 +59,27 @@
         </b-col>
         <b-col cols="12" md="6" lg="5" class="text-right">
           <img src="../../static/image/slider-01.png" alt="" class="img-fluid" />
-        </b-col>
+        </b-col> -->
       </b-row>
     </b-container>
   </section>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        slide: 0,
+        sliding: null
+      }
+    },
+    methods: {
+      onSlideStart(slide) {
+        this.sliding = true
+      },
+      onSlideEnd(slide) {
+        this.sliding = false
+      }
+    }
+  }
+</script>
