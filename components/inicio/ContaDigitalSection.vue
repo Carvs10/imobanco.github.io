b8b419ed4c6104f2c75c924917fd306ceb49159a
Phelipe Queiroz <phelipedaniel84@gmail.com>
2021-06-23 15:54:13 -0300

    Animação slide css. inicio.

--- components/inicio/ContaDigitalSection.vue
+++ components/inicio/ContaDigitalSection.vue
@@ -1,13 +1,36 @@
 <template>
   <section class="slide-home">
-      <div class="slide-home-text">
+    <b-container>
+      <b-row>
+      <b-col lg=6 class="slide-home-text">
+        <h1 class="text-slide-home">Recebimentos e cobranças <br> em um único lugar.</h1>
+        <img src="../../static/image/boleto-texto.png" alt="" class="" />    
+      <div class="mt-2">
+          <a href="https://app.imobanco.com.br/register" target="_blank" class="btn btn-success">Abra sua conta</a>
+      </div>
+      </b-col>
+      <b-col lg=6 class="slide-home-text-right">
+        <div class="globo-slide"></div>
+        <div class="globo-barra"></div>
+      </b-col>
+    </b-row>
+    </b-container>
+      <!-- <div class="slide-home-text">
       <h1 class="text-slide-home">Recebimentos e cobranças <br> em um único lugar.</h1>
       <img src="../../static/image/boleto-texto.png" alt="" class="" />    
       <div class="mt-2">
-         <!-- <p class="text-subtitle ">Simplifique com o Imobanco.</p> -->
+
           <a href="https://app.imobanco.com.br/register" target="_blank" class="btn btn-success">Abra sua conta</a>
       </div>
     </div>
+    <div class="slide-home-text">
+      <h1 class="text-slide-home">Recebimentos e cobranças <br> em um único lugar.</h1>
+      <img src="../../static/image/boleto-texto.png" alt="" class="" />    
+      <div class="mt-2">
+
+          <a href="https://app.imobanco.com.br/register" target="_blank" class="btn btn-success">Abra sua conta</a>
+      </div>
+    </div> -->
      <div>
         <img src="../../static/image/bg-home-1.png" alt="" class="waves" />
      </div>     
