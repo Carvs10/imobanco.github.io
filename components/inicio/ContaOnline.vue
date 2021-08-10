92c45a8c764dc40ac227be1edda6b6d2859a4e90
phelipequeiroz <phelipedaniel84@gmail.com>
2021-08-10 05:47:59 -0300

    Conta Online new version

--- components/inicio/ContaOnline.vue
+++ components/inicio/ContaOnline.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="col-lg-6 text-left">
         <h2 class="text-primary title-font">Conta Digital</h2>
-        <h4 class=" text-primary text-content">Com o Imobanco você gera, envia e administra todos os seus recebimentos e pagamentos.  Tudo online e em tempo real, de qualquer lugar do Brasil ou do mundo.</h4>
+        <h5 class=" text-primary text-content">Com o Imobanco você gera, envia e administra todos os seus recebimentos e pagamentos.  Tudo online e em tempo real, de qualquer lugar do Brasil ou do mundo.</h5>
         <br>
         <h5 class="text-success"><i class="fas fa-home text-center icon-seguro"></i> <span class="text-primary text-title">Conta Digital</span></h5>
         <p class="text-secundary">Gere boletos, carnês e acompanhe todo o processamento dos seus recebimentos.</p>
